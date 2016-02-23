dbb9103e0074979fa3c7c613bf83fc64f442c959
Ran Gao <gaoran1994@gmail.com>
2016-02-23 13:42:02 -0500

    first prototype

--- popup.js
+++ popup.js
@@ -1,7 +1,14 @@
 //script for popup.html
+window.onload = function(){
+  var start_button = document.getElementById("start");
+  start_button.onclick = function(){
+    startTimer();
+  }
+}
 
-function startTimer(){
-    var time_select = document.getElementById("times");
-    var time = time_select.options[time_select.selectedIndex].value;
-    alert("enter start timer" + time);
+function startTimer() {
+  var time_select = document.getElementById("times");
+  var time = time_select.options[time_select.selectedIndex].value;
+  var msg = { title: "startTimer", value: time };
+  chrome.runtime.sendMessage(msg);
 }
