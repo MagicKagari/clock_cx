cef1d39ef23a8bde427f38c755d092cd31435c38
Ran Gao <gaoran1994@gmail.com>
2016-02-23 15:47:40 -0500

    add chinese version and more function

--- background.js
+++ background.js
@@ -1,21 +1,61 @@
 //script for background.js
 chrome.extension.onMessage.addListener(dispatchMessage);
+var is_timer_start = false;
+var intervalID;
+var current_timer;
 
 function dispatchMessage(msg, sender, sendResponse) {
-  if (msg.title === "startTimer") {
-    console.log(msg.value);
-    showNotification();
-  } else {
-    console.log("unknown message");
+  switch(msg.title) {
+    case "startTimer":
+      setupTimer(msg.value);
+      current_timer = msg.value;
+      is_timer_start = true;
+      break;
+    case "cleanTimer":
+      cleanTimer();
+      is_timer_start = false;
+      break;
+    default:
+      console.log("unknown message" + msg.title);
   }
 }
 
+function setupTimer(time) {
+  var ms;
+  switch(time) {
+    case "1m":
+      ms = 1*60*1000;
+      break;
+    case "10m":
+      ms = 10*60*1000;
+      break;
+    case "15m":
+      ms = 15*60*1000;
+      break;
+    case "20m":
+      ms = 20*60*1000;
+      break;
+    case "30m":
+      ms = 30*60*1000;
+      break;
+    default:
+      console.log("unknown time" + time);
+      ms = 60*1000;
+  }
+  intervalID = setInterval(function(){ showNotification();}, ms);
+}
+
+function cleanTimer() {
+  clearInterval(intervalID);
+}
 
 function showNotification() {
+    var _title = chrome.i18n.getMessage("notificationTitle");
+    var _body = chrome.i18n.getMessage("notificationBody");
     var opt = {
         type: "basic",
-        title: "Get up",
-        message: "Time to exercise",
+        title: _title,
+        message: _body,
         iconUrl: "icon.png"
     }
     chrome.notifications.create(opt);
--- popup.js
+++ popup.js
@@ -1,14 +1,72 @@
 //script for popup.html
+var bg_page = chrome.extension.getBackgroundPage();
+
 window.onload = function(){
+  localize();
+  setupView();
+};
+
+function setupView() {
   var start_button = document.getElementById("start");
-  start_button.onclick = function(){
-    startTimer();
+  var cancel_button = document.getElementById("cancel");
+  var start_info = document.getElementById("start_info");
+  var cancel_info = document.getElementById("cancel_info");
+  var times = document.getElementById("times");
+
+  if (bg_page.is_timer_start) {
+    start_button.style.visibility = "hidden";
+    start_info.style.visibility = "hidden";
+    times.style.visibility = "hidden";
+
+    var current_time = bg_page.current_timer;
+    cancel_info.innerHTML = cancel_info.innerHTML + chrome.i18n.getMessage(current_time+"Value");
+
+    cancel_button.onclick = function(){
+      cleanTimer();
+      location.reload();
+    };
+  } else {
+    cancel_button.style.visibility = "hidden";
+    cancel_info.style.visibility = "hidden";
+    start_button.onclick = function(){
+      startTimer();
+      location.reload();
+    };
   }
+}
+
+function localize() {
+  var header = document.getElementById("header");
+  var start_info = document.getElementById("start_info");
+  var cancel_info = document.getElementById("cancel_info");
+  var s1m = document.getElementById("1m");
+  var s10m = document.getElementById("10m");
+  var s15m = document.getElementById("15m");
+  var s20m = document.getElementById("20m");
+  var s30m = document.getElementById("30m");
+  var start = document.getElementById("start");
+  var cancel = document.getElementById("cancel");
+
+  header.innerHTML = chrome.i18n.getMessage("headerValue");
+  start_info.innerHTML = chrome.i18n.getMessage("start_infoValue");
+  cancel_info.innerHTML = chrome.i18n.getMessage("cancel_infoValue");
+  s1m.innerHTML = chrome.i18n.getMessage("1mValue");
+  s10m.innerHTML = chrome.i18n.getMessage("10mValue");
+  s15m.innerHTML = chrome.i18n.getMessage("15mValue");
+  s20m.innerHTML = chrome.i18n.getMessage("20mValue");
+  s30m.innerHTML = chrome.i18n.getMessage("30mValue");
+  start.innerHTML = chrome.i18n.getMessage("startValue");
+  cancel.innerHTML = chrome.i18n.getMessage("cancelValue");
 }
 
 function startTimer() {
   var time_select = document.getElementById("times");
   var time = time_select.options[time_select.selectedIndex].value;
   var msg = { title: "startTimer", value: time };
+  chrome.runtime.sendMessage(msg);
+}
+
+function cleanTimer() {
+  var msg = { title: "cleanTimer"};
   chrome.runtime.sendMessage(msg);
 }
